ccc1a388dc37c237e7fd9dc6cd1f55e165c25195
nishant30 <goyalnishant30@gmail.com>
2024-08-13 00:03:04 +0530

    heirarchy of tags created using react createElement

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
-const heading = React.createElement("h1",{id:"heading"},"Hello World From React");
+/**
+ * 
+ * <div id="parent">
+ *   <div id = "child">
+ *    <h1> I am H1 Tag</h1>
+ *    <h2> I am h2 tag</h2>
+ *   </div>
+ * </div>
+ */
 
-console.log(heading);
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
+    )); 
+
+
+
+console.log(parent); // object 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
